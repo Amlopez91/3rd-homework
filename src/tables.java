65f8b57a6e54f2fa02761666e0ceb072b41c15ae
Amlopez91 <anamiguezlopez@gmail.com>
2017-05-16 18:27:35 +0200

    fourth commit

--- src/tables.java
+++ src/tables.java
@@ -31,5 +31,30 @@ public class tables
             }
             System.out.println();
         }
+        
+        //ordenar
+        for (int i=0;i<6;i++){
+            for(int j=0;j<2;j++){
+                for (int x=0;x<6;x++){
+                    for(int y=0;y<2;y++){
+                        if(table[i][j] < table[x][y]){
+                            int t = table[i][j];
+                            table [i][j] = table [x][y];
+                            table[x][y] = t;
+                        }
+                    }
+                }
+            }
+        }
+        
+        System.out.print("-----------"+"\n");
+        for (int i=0;i<6;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                System.out.print(table[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
