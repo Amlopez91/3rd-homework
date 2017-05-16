d70deafe45ae9107680b858761e08107eea15a61
Amlopez91 <anamiguezlopez@gmail.com>
2017-05-16 18:20:53 +0200

    thrid commit

--- src/tables.java
+++ src/tables.java
@@ -15,18 +15,13 @@ public class tables
         int table [][];
         table = new int [6][2];
         
-        table[0][0] = 1;
-        table[0][1] = 2;
-        table[1][0] = 3;
-        table[1][1] = 4;
-        table[2][0] = 5;
-        table[2][1] = 6;
-        table[3][0] = 7;
-        table[3][1] = 8;
-        table[4][0] = 9;
-        table[4][1] = 10;
-        table[5][0] = 11;
-        table[5][1] = 12;
+        for (int i=0;i<6;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                table [i][j] = (int) (Math.random()*50);
+            }
+        }
         
         for (int i=0;i<6;i++)
         {
